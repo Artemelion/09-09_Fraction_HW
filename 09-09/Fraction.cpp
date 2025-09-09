1824000f9a0ed7115f311a1cc7bf1d5223c6a7db
Artemeleon Morarevich <artyommorar@gmail.com>
2025-09-09 14:22:28 +0300

    class Fraction is completed. Used MultiFile and ptr this

--- 09-09/Fraction.cpp
+++ 09-09/Fraction.cpp
@@ -1,1 +1,65 @@
 #include "Fraction.h"
+#include <iostream>
+using namespace std;
+
+void Fraction::Init()
+{
+	cout << "Enter numerator: ";
+	cin >> numerator;
+	cout << "Enter denominator: ";
+	cin >> denominator;
+}
+
+void Fraction::Init(int _numerator, int _denominator)
+{
+	numerator = _numerator;
+	denominator = _denominator;
+}
+
+void Fraction::Print()
+{
+	cout << numerator << "/" << denominator << endl;
+}
+
+Fraction Fraction::Add(Fraction& b)
+{
+	Fraction result;
+	result.numerator = this->numerator * b.denominator + b.numerator * this->denominator;
+	result.denominator = this->denominator * b.denominator;
+	return result;
+}
+
+Fraction Fraction::Sub(Fraction& b)
+{
+	Fraction result;
+	result.numerator = this->numerator * b.denominator - b.numerator * this->denominator;
+	result.denominator = this->denominator * b.denominator;
+	return result;
+}
+
+Fraction Fraction::Mul(Fraction& b)
+{
+	Fraction result;
+	result.numerator = this->numerator * b.numerator;
+	result.denominator = this->denominator * b.denominator;
+	return result;
+}
+
+Fraction Fraction::Div(Fraction& b)
+{
+	Fraction result;
+	result.numerator = this->numerator * b.denominator;
+	if (this->denominator == 0 || b.denominator == 0) 
+	{
+		cout << "Error: No need zero in denominator plz" << endl;
+	}
+	else {
+		result.denominator = this->denominator * b.numerator;
+	}
+
+return result;
+
+}
+
+
+
--- 09-09/Main.cpp
+++ 09-09/Main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include "Fraction.h"
 using namespace std;
 
 int main()
 {
+	Fraction a, b;
+	a.Init(1, 2);
+	b.Init(3, 4);
+
+	a.Print(); b.Print();
+
+	Fraction result;
+	result = a.Add(b);
+	result.Print();
+
+	result = a.Sub(b);
+	result.Print();
+
+	result = a.Mul(b);
+	result.Print();
+
+	result = a.Div(b);
+	result.Print();
+	
 
 	return 0;
 }
